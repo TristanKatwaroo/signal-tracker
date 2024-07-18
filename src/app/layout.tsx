a85d8364ad88bc7a3e281d389eeba516d5d2ff50
Tristan Katwaroo <katwaroo.tristan@gmail.com>
2024-07-18 00:56:31 -0400

    forced input text size 16px for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      </head>
       <body
         className={cn(fontSans.className, {
           "debug-screens": process.env.NODE_ENV === "development",
