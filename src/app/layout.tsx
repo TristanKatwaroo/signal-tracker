454d2c118cda7572c68d0e3f538a0bc71bbc879b
Tristan Katwaroo <katwaroo.tristan@gmail.com>
2024-07-18 00:49:14 -0400

    fix for mobile input zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body
         className={cn(fontSans.className, {
           "debug-screens": process.env.NODE_ENV === "development",
