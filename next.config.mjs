b2ebcbbf20a608a464fad8df2d932bf378bfeff4
Tristan Katwaroo <katwaroo.tristan@gmail.com>
2024-07-21 13:59:11 -0400

    modified sentry config

--- next.config.mjs
+++ next.config.mjs
@@ -1,66 +1,71 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import { withSentryConfig } from '@sentry/nextjs';
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-// (when running the application with `next dev`), for more information see:
-// https://github.com/cloudflare/next-on-pages/blob/5712c57ea7/internal-packages/next-dev/README.md
 if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform();
 }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, dev }) => {
     if (!isServer) {
       config.optimization.splitChunks = {
         chunks: 'all',
         cacheGroups: {
+          default: false,
+          vendors: false,
           vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
+            name: 'vendor',
             chunks: 'all',
+            test: /node_modules/,
+            priority: 20,
+          },
+          common: {
+            name: 'common',
+            minChunks: 2,
+            chunks: 'all',
+            priority: 10,
+            reuseExistingChunk: true,
+            enforce: true,
           },
         },
       };
     }
+
+    // Enable tree shaking and dead code elimination
+    config.optimization.usedExports = true;
+
+    if (!dev) {
+      // Enable full optimization in production
+      config.optimization.minimize = true;
+    }
+
     return config;
   },
-  optimization: {
-    minimize: true,
+  experimental: {
+    optimizeCss: true, // This will enable CSS optimization
   },
 };
 
-export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+const sentryWebpackPluginOptions = {
+  org: "tempest-interactive",
+  project: "javascript-nextjs",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+  // New options to reduce bundle size
+  include: './dist',
+  ignoreFile: '.sentrycliignore',
+  ignore: ['node_modules', 'webpack.config.js'],
+  configFile: 'sentry.properties',
+  stripPrefix: ['dist/'],
+  urlPrefix: '~/_next',
+  // Enable tree-shaking for Sentry
+  release: process.env.SENTRY_RELEASE,
+  cleanArtifacts: true,
+};
 
-org: "tempest-interactive",
-project: "javascript-nextjs",
-
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
-
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
-
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
