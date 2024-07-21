d60ec941b9f1fbc1fa5070c5a67121614aa9cfe5
Tristan Katwaroo <katwaroo.tristan@gmail.com>
2024-07-21 14:27:43 -0400

    disable optimization.usedExports

--- next.config.mjs
+++ next.config.mjs
@@ -8,61 +8,90 @@ if (process.env.NODE_ENV === 'development') {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer, dev }) => {
+    // Optimize client-side bundles
     if (!isServer) {
       config.optimization.splitChunks = {
         chunks: 'all',
+        maxInitialRequests: 25,
+        minSize: 20000,
         cacheGroups: {
           default: false,
           vendors: false,
-          vendor: {
-            name: 'vendor',
+          framework: {
             chunks: 'all',
-            test: /node_modules/,
+            name: 'framework',
+            test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
+            priority: 40,
+            enforce: true,
+          },
+          lib: {
+            test(module) {
+              return module.size() > 160000 &&
+                /node_modules[/\\]/.test(module.identifier());
+            },
+            name(module) {
+              const hash = crypto.createHash('sha1');
+              hash.update(module.identifier());
+              return hash.digest('hex').substring(0, 8);
+            },
+            priority: 30,
+            minChunks: 1,
+            reuseExistingChunk: true,
+          },
+          commons: {
+            name: 'commons',
+            minChunks: 2,
             priority: 20,
           },
-          common: {
-            name: 'common',
-            minChunks: 2,
-            chunks: 'all',
+          shared: {
+            name(module, chunks) {
+              return crypto
+                .createHash('sha1')
+                .update(
+                  chunks.reduce((acc, chunk) => acc + chunk.name, '')
+                )
+                .digest('hex') + (dev ? '-dev' : '-prod');
+            },
             priority: 10,
+            minChunks: 2,
             reuseExistingChunk: true,
-            enforce: true,
           },
         },
       };
-    }
 
-    // Enable tree shaking and dead code elimination
-    // config.optimization.usedExports = true;
+      // Remove the problematic optimization.usedExports
+      // config.optimization.usedExports = true;
 
-    if (!dev) {
-      // Enable full optimization in production
-      config.optimization.minimize = true;
+      if (!dev) {
+        config.optimization.minimize = true;
+        // Make sure TerserPlugin is imported if you're using it
+        // const TerserPlugin = require('terser-webpack-plugin');
+        // config.optimization.minimizer.push(
+        //   new TerserPlugin({
+        //     terserOptions: {
+        //       compress: {
+        //         drop_console: true,
+        //       },
+        //       mangle: true,
+        //     },
+        //   })
+        // );
+      }
     }
 
     return config;
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 const sentryWebpackPluginOptions = {
-  org: "tempest-interactive",
-  project: "javascript-nextjs",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  tunnelRoute: "/monitoring",
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-  // New options to reduce bundle size
-  include: './dist',
-  ignoreFile: '.sentrycliignore',
-  ignore: ['node_modules', 'webpack.config.js'],
-  configFile: 'sentry.properties',
-  stripPrefix: ['dist/'],
-  urlPrefix: '~/_next',
-  // Enable tree-shaking for Sentry
-  release: process.env.SENTRY_RELEASE,
-  cleanArtifacts: true,
+  silent: true,
+  // Sentry options here
 };
 
-export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+// Only use Sentry in production
+export default process.env.NODE_ENV === 'production'
+  ? withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+  : nextConfig;
