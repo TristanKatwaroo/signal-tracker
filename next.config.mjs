fb847bccffa4551f009d2103c01136e160774dab
Tristan Katwaroo <katwaroo.tristan@gmail.com>
2024-07-21 15:25:13 -0400

    removed sentry integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import {withSentryConfig} from '@sentry/nextjs';
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
@@ -9,65 +8,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-          },
-        },
-      };
-    }
-    return config;
-  },
-  optimization: {
-    minimize: true,
-  },
-};
+const nextConfig = {};
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  org: "tempest-interactive",
-  project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
-  silent: true,
-
-  dryRun: process.env.NODE_ENV !== 'production',
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: false,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: false,
-
-  autoInstrumentServerFunctions: false,
-  autoInstrumentPages: false,
-  disableClientWebpackPlugin: true, // Disables client-side Webpack plugin
-  disableServerWebpackPlugin: true, // Disables server-side Webpack plugin
-});
+export default nextConfig;
