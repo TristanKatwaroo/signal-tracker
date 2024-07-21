ca9504d501d298ab79f7761b98bab05e6f098cac
Tristan Katwaroo <katwaroo.tristan@gmail.com>
2024-07-21 19:55:17 -0400

    styling update

--- src/components/signals/ResultPill.tsx
+++ src/components/signals/ResultPill.tsx
@@ -9,7 +9,7 @@ type ResultPillProps = {
 const ResultPill: React.FC<ResultPillProps> = ({ pity, name }) => {
   return (
     <div className="relative flex items-center bg-primary/20 rounded-full pl-6 pr-4 py-1">
-      <div className="absolute -left-0 flex items-center justify-center h-6 w-6 rounded-full bg-primary text-sm font-semibold z-10">
+      <div className="absolute -left-0 flex items-center justify-center h-6 w-6 rounded-full bg-primary text-xs font-semibold z-10">
         {pity}
       </div>
       <span className="ml-2 text-xs">{name}</span>
