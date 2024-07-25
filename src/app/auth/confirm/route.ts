f6b838ac893b73c6a8027ecbb1cb1857dfc61139
Tristan Katwaroo <katwaroo.tristan@gmail.com>
2024-07-25 00:00:43 -0400

    updated auth confirm route

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,16 +1,14 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
-import { cookies } from 'next/headers'
-import { NextRequest, NextResponse } from 'next/server'
-// The client you created from the Server-Side Auth instructions
+import { type NextRequest, NextResponse } from 'next/server'
+
 import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
   const next = searchParams.get('next') ?? '/'
-  const redirectTo = request.nextUrl.clone()
-  redirectTo.pathname = next
 
   if (token_hash && type) {
     const supabase = createClient()
@@ -20,11 +18,11 @@ export async function GET(request: NextRequest) {
       token_hash,
     })
     if (!error) {
-      return NextResponse.redirect(redirectTo)
+      // redirect user to specified redirect URL or root of app
+      redirect(next)
     }
   }
 
-  // return the user to an error page with some instructions
-  redirectTo.pathname = '/auth/auth-code-error'
-  return NextResponse.redirect(redirectTo)
+  // redirect the user to an error page with some instructions
+  redirect('/error')
 }
