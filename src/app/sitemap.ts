09e5a53d211108cc4efa1f64557bcb57bef68292
Tristan Katwaroo <katwaroo.tristan@gmail.com>
2024-08-29 17:21:17 -0400

    disabled lastModified for SEO accuracy

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,17 +1,17 @@
 import type { MetadataRoute } from 'next'
  
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = 'https://signaltracker.gg'
 
     return [
         {
             url: baseUrl,
-            lastModified: new Date(),
+            // lastModified: new Date(),
             priority: 1,
         },
         {
             url: `${baseUrl}/signals`,
-            lastModified: new Date(),
+            // lastModified: new Date(),
             priority: 0.8,
         },
     ]
