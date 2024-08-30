5d531accd9b943281c15fb70b710267e43583d8a
Tristan Katwaroo <katwarot@sheridancollege.ca>
2024-08-30 15:08:30 -0400

    add main logos to imagesConfig

--- src/lib/imagesConfig.ts
+++ src/lib/imagesConfig.ts
@@ -7,8 +7,9 @@ if (!accountHash) {
 // Define image paths relative to the Cloudflare Image Delivery URL
 const imagesConfig: { [key: string]: any } = {
   logos: {
-    main: `${accountHash}/357c2697-28a5-4c60-4e94-150e3847ab00/public`,
+    main: `${accountHash}/4413ed06-2f51-4678-551d-685e91abd200/public`,
     secondary: `${accountHash}/example-image-id/variant`,
+    transparent: `${accountHash}/b0f0c62e-a70b-4e51-ecd2-0828d0818c00/public`,
   },
   bangboos: {
     bangboo1half: `${accountHash}/357c2697-28a5-4c60-4e94-150e3847ab00/public`,
